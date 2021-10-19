0bf37f517efa829be5d971121905c7722b976d71
Greg Bolet <gbolet@vt.edu>
2021-10-19 19:05:26 -0400

    started sample socket code

--- controller/main.py
+++ controller/main.py
@@ -1,7 +1,33 @@
 #!/usr/bin/python3
 
+import socket
+
+HOST = 'controller.laceynet'
+PORT = 65432
+
 def main():
     print("Hello from Controller!")
+
+    # AF_INET is IPV4, SOCK_STREAM is for TCP protocol
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        # Associate the given HOST with the given PORT
+        s.bind((HOST, PORT))
+
+        # Make this a listening server
+        s.listen()
+
+        # Block and wait for an incoming connection
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                # Blocking calls
+                data = conn.recv(1024)
+
+                # If no bytes are received, connection is closed
+                if not data:
+                    break
+                conn.sendall(data)
 
     return
 
--- worker/main.py
+++ worker/main.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 
+import socket
+
+HOST = 'controller.laceynet'
+PORT = 65432
+
 def main():
     print("Hello from Worker!")
+    
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(b'Hello, world')
+        data = s.recv(1024)
+        print('Received', repr(data))
 
     return
 
