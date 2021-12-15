3fe4c1071015760875564cfef0cf7aa5dba58221
gregbolet <gbolet@vt.edu>
2021-12-15 16:32:30 -0500

    added cookies

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-const cool = require('cool-ascii-faces');
+const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const cookieParser = require('cookie-parser')
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(cookieParser());
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+app.get('/cool', (req, res) => res.send(cool()))
+
+app.get('/cookieLand', (req,res) => {
+	res.send('my simple http server');
+})
+
+//a get route for adding a cookie
+
+app.get('/setcookie', (req, res) => {
+	res.cookie(`Cookie token name`,`encrypted cookie string Value`,{
+		maxAge: 5000,
+		// expires works the same as the maxAge
+		expires: new Date('01 12 2021'),
+		secure: true,
+		httpOnly: true,
+		sameSite: 'lax'
+	});
+	res.send('Cookie have been saved successfully');
+});
+
+// get the cookie incoming request
+app.get('/getcookie', (req, res) => {
+	//show the saved cookies
+	console.log(req.cookies)
+	res.send(req.cookies);
+});
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
