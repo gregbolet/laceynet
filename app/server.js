444b6a1f0afdf3eb9ae6fb2be6df648b330ec23b
gregbolet <gbolet@vt.edu>
2021-12-29 16:55:08 -0500

    starting websockets using socket.io

--- app/server.js
+++ app/server.js
@@ -1,21 +1,47 @@
 const express = require('express');
-const http = require('http');
-const WebSocket = require('ws');
+//const WebSocket = require('ws');
+const socketIO = require('socket.io');
 
-const port = 6969;
-const server = http.createServer(express);
-const wss = new WebSocket.Server({ server })
+const PORT = process.env.PORT || 5000;
+const app = express();
 
-wss.on('connection', function connection(ws) {
-	ws.on('message', function incoming(data) {
-		wss.clients.forEach(function each(client) {
-			if (client !== ws && client.readyState === WebSocket.OPEN) {
-				client.send(data);
-			}
-		})
-	})
-})
+// Serve all the files from the /public folder
+// Given public/index.html it will be served from myaddress.com/index.html
+app.use(express.static('public'))
 
-server.listen(port, function() {
-	console.log(`Server is listening on ${port}!`)
-})
+const server = app.listen(PORT, function() {
+  console.log(`Example app listening on port ${PORT}!`)
+});
+
+// socketIO will server the websockets and handle reverse
+// compatibility and fallback
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('Client connected');
+    socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+// Update the page time every second
+setInterval(() => {
+  io.emit('time', new Date().toTimeString());
+} , 1000);
+
+
+//// Set up a websocket server
+//const wss = new WebSocket.Server({ server });
+//
+//// Handle the incoming websocket connection
+//wss.on('connection', socket => {
+//  console.log('Client connected!');
+//  socket.on('message', message => console.log(message));
+//  socket.on('close', () => console.log('Client disconnected'));
+//});
+
+// Update the time each second on the client
+//setInterval(() => {
+//    wss.clients.forEach((client) => {
+//          client.send(new Date().toTimeString());
+//        });
+//}, 1000);
+
