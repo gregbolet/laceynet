68e8993f7d0e6492fa1514e6c548f062b20fe7e4
gregbolet <gbolet@vt.edu>
2021-12-30 00:44:11 -0500

    started client list

--- app/server.js
+++ app/server.js
@@ -13,13 +13,28 @@ const server = app.listen(PORT, function() {
   console.log(`Example app listening on port ${PORT}!`)
 });
 
-// socketIO will server the websockets and handle reverse
+// socketIO will serve the websockets and handle reverse
 // compatibility and fallback
 const io = socketIO(server);
 
+// This will map socketIDs (i.e: clients) to their assigned game numbers
+let clients = new Map();
+
 io.on('connection', (socket) => {
-    console.log('Client connected');
-    socket.on('disconnect', () => console.log('Client disconnected'));
+  console.info(`Client connected [id=${socket.id}]`);
+
+  // Add the client to the client list, keep their game info
+  clients.set(socket, {nums: [], currIdx: -1});
+
+  socket.on('disconnect', () => {
+	  console.log('Client disconnected');
+	  clients.delete(socket);
+  });
+
+  socket.on('clicked', (msg) => {
+	console.log(`playerid ${socket.id}, msg: `, msg);	
+  });
+
 });
 
 // Update the page time every second
@@ -27,21 +42,4 @@ setInterval(() => {
   io.emit('time', new Date().toTimeString());
 } , 1000);
 
-
-//// Set up a websocket server
-//const wss = new WebSocket.Server({ server });
-//
-//// Handle the incoming websocket connection
-//wss.on('connection', socket => {
-//  console.log('Client connected!');
-//  socket.on('message', message => console.log(message));
-//  socket.on('close', () => console.log('Client disconnected'));
-//});
-
-// Update the time each second on the client
-//setInterval(() => {
-//    wss.clients.forEach((client) => {
-//          client.send(new Date().toTimeString());
-//        });
-//}, 1000);
 
