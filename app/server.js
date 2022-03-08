739b02a23c29660950f3f9a377117676c157cadc
Skylar Liang <skylarl@vt.edu>
2022-03-07 21:28:05 -0500

    u

--- app/server.js
+++ app/server.js
@@ -55,7 +55,7 @@ app.get('/measure', (req,res)=>{
   console.info(req.headers);
   var mac = req.headers['MEASURE'];
   // find the corresponding measurement from table?
-  let text = '{ "a":11.1 , "B":22.2,  "c":33.3, "D":44.4, "e":55.5, "F":66.6}';
+  let text = '{ \"a\":11.1 , \"B\":22.2,  \"c\":33.3, \"D\":44.4, \"e\":55.5, \"F\":66.6}';
   const jsonObj = JSON.parse(text);
   console.info(jsonObj);
   res.send(jsonObj);
